c525fcfd22b8667f125846f9ee3d2b02405f99ad
ydr29913 <ydr29913@educastur.es>
2024-12-12 14:23:34 +0100

    Añadida Base de Datos Final

--- tarea3dwesydr29913/src/main/resources/tarea2dwesdanielgayol.sql
+++ tarea3dwesydr29913/src/main/resources/tarea2dwesdanielgayol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-12-2024 a las 14:18:21
+-- Tiempo de generación: 12-12-2024 a las 14:22:18
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tarea2dwesdanielgayol`
 --
+CREATE DATABASE IF NOT EXISTS `tarea2dwesdanielgayol` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tarea2dwesdanielgayol`;
 
 -- --------------------------------------------------------
 
